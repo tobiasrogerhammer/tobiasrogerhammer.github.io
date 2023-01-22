a66758372bd465c19777d2d104d1e7b46ec778f6
thammer <tobias@hammerhome.no>
2023-01-22 15:04:21 +0100

    improved

--- reactionTest/JS/test.js
+++ reactionTest/JS/test.js
@@ -1,66 +1,63 @@
-let x;
-let randomNum;
-let countDown = true;
+let countdownTimer;
+let randomCountdown;
+let isCountingDown = true;
 let elapsedTime = 0;
-let rounds = 0;
+let totalRounds = 0;
 
 function startGame() {
     document.body.classList.add("wait");
     document.getElementById("timer").innerHTML = 'Wait for green!';
     document.getElementById("explanation").style.display = "none";
-
-    randomNum = 0;
-    countDown = true;
+    randomCountdown = 0;
+    isCountingDown = true;
     elapsedTime = 0;
 
     document.getElementById("stopbutton").disabled = true;
-    randomNum = Math.floor(Math.random() * 1000) + 1;
-    x = setInterval(updateTimer, 1);
+    randomCountdown = Math.floor(Math.random() * 4000) + 1000; // count down between 2 and 8 seconds
+    countdownTimer = setInterval(updateTimer, 1);
     document.getElementById("start").style.display = "none";
     document.getElementById("stopbutton").style.display = "block";
-    rounds++
+    totalRounds++
 
-    if (rounds === 4) {
+    if (totalRounds === 4) {
         document.getElementById("bestScore").innerHTML = 'Tries:  4/5';
-    } else if (rounds === 3){
+    } else if (totalRounds === 3) {
         document.getElementById("bestScore").innerHTML = 'Tries:  3/5';
-    } else if (rounds === 2){
+    } else if (totalRounds === 2) {
         document.getElementById("bestScore").innerHTML = 'Tries:  2/5';
-    } else if (rounds === 1){
+    } else if (totalRounds === 1) {
         document.getElementById("bestScore").innerHTML = 'Tries:  1/5';
     }
 }
 
 function updateTimer() {
-    if (countDown) {
-        randomNum--;
-        if (randomNum < 0) {
-            countDown = false;
-            randomNum = 0;
+    if (isCountingDown) {
+        randomCountdown--;
+        if (randomCountdown < 0) {
+            isCountingDown = false;
+            randomCountdown = 0;
+            startTime = performance.now(); // start counting milliseconds
+            document.getElementById("stopbutton").disabled = false;
         }
     } else {
-        randomNum++;
+        elapsedTime = performance.now() - startTime;
     }
-
-    if (randomNum === 0) {
+    if (randomCountdown === 0) {
         document.body.classList.remove("wait");
         document.body.classList.add("tap");
-        document.getElementById("stopbutton").disabled = false;
     }
 }
-
 
 let reactionTimes = {};
 
 function stopTimer() {
-    if (rounds === 5) {
+    if (totalRounds === 5) {
         document.getElementById("timer").innerHTML = 'Congratulations, round over!';
         document.getElementById("start").style.display = "none";
         document.getElementById("stopbutton").style.display = "none";
         document.getElementById("average").style.display = "block";
         document.body.classList.remove("tap");
         document.body.classList.remove("wait");
-
         if (average < bestRound) {
             bestRound = average;
             document.getElementById("bestScore").innerHTML = 'Best average:  ' + bestRound + ' ms';
@@ -68,50 +65,43 @@ function stopTimer() {
         }
 
     } else {
-        document.getElementById("timer").innerHTML = randomNum + 'ms reaction time';
-        clearInterval(x);
+        document.getElementById("timer").innerHTML = `Reaction time: ${elapsedTime.toFixed(0)}ms`;
+        clearInterval(countdownTimer);
         document.getElementById("start").style.display = "block";
+
         document.getElementById("stopbutton").style.display = "none";
         document.getElementById("average").style.display = "block";
         document.body.classList.remove("tap");
         document.body.classList.remove("wait");
-
         let date = new Date();
         let dateString = date.toUTCString();
-        reactionTimes[dateString] = randomNum;
+        reactionTimes[dateString] = randomCountdown;
         localStorage.setItem("reactionTimes", JSON.stringify(reactionTimes));
     }
+    setAverageReactionTime();
 }
 
-
-let average = 0;
-let bestRound = 10000;
+let sum = 0;
+let round = 0;
 function setAverageReactionTime() {
-    let values = Object.values(reactionTimes);
-    let sum = values.reduce((accumulator, currentValue) => accumulator + currentValue);
-    average = sum / values.length;
-    average = average.toFixed(0);
-    document.getElementById("average").innerHTML = 'Average:  ' + average + 'ms';
+    sum += elapsedTime;
+    round++;
+    let average = sum / round;
+    document.getElementById("average").innerHTML = 'Average: ' + average.toFixed(0) + 'ms';
 }
-
-
 
 
 function resetGame() {
-    clearInterval(x); // stop the timer
-    average = 0;
-    rounds = 0;
-    randomNum = 0;
-    countDown = true;
-    elapsedTime = 0;
-    reactionTimes = [];
-    document.getElementById("average").innerHTML = 'Average of 5:';
+    clearInterval(countdownTimer); // stop the timer
+    averageReactionTime = 0;
+    totalRounds = 0;
+    randomCountdown = 0;
+    document.getElementById("timer").innerHTML = "";
+    document.getElementById("bestScore").innerHTML = bestRoundTime;
+    document.getElementById("average").innerHTML = "";
+    reactionTimes = {};
+    localStorage.removeItem("reactionTimes");
     document.getElementById("start").style.display = "block";
-    document.getElementById("timer").innerHTML = '';
-    document.getElementById("stopbutton").style.display = "none";
+    document.getElementById("average").style.display = "none";
     document.getElementById("explanation").style.display = "block";
-    document.getElementById("bestScore").innerHTML = 'Best average:  ' + bestRound + 'ms';
-    document.body.classList.remove("tap");
-    document.body.classList.remove("wait");
 }
-
