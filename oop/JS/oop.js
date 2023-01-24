49071d7f845c2a3a01709825f25e8aa3457ccf0d
thammer <tobias@hammerhome.no>
2023-01-24 22:54:42 +0100

    mostly css

--- oop/JS/oop.js
+++ oop/JS/oop.js
@@ -1,48 +1,55 @@
 class DinAlder {
-    constructor(navn, fodselsar){
-      this.navn = navn;
-      this.fodselsar = fodselsar;
-    }
-    calculateAge() {
-      if (this.fodselsar > 2023) {
-        return 'Du er ikke født enda';
-      }
-      const currentYear = new Date().getFullYear();
-      return currentYear - this.fodselsar;
-    }
-    under(){
-        const age = this.calculateAge();
-        return age <=15;
-    }
-    mellom() {
-      const age = this.calculateAge();
-      return age >= 15 && age <= 18;
-    }
-    over(){
-        const age = this.calculateAge();
-        return age >= 18;
-      }
+  constructor(navn, fodselsar) {
+    this.navn = navn;
+    this.fodselsar = fodselsar;
   }
-  
-  document.getElementById('calculatorInput').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const fname = document.getElementById('fname').value;
-    const fodselsar = document.getElementById('fodselsar').value;
-    const nyAlder = new DinAlder(fname, fodselsar);
-    const age = nyAlder.calculateAge();
-  
-    let message = '';
-    if (nyAlder.mellom()) {
-      message = fname + ' er eldre enn den kriminelle lavalder';
+  calculateAge() {
+    if (this.fodselsar > 2023) {
+      return 'Du er ikke født enda';
     }
-    if (nyAlder.under()) {
-        message = fname + ' er yngre enn den kriminelle lavalder';
-      }
-      if (nyAlder.over()) {
-        message = fname + ' er over enn den kriminelle lavalder og du har stemmerett';
-      }
-  
-    document.getElementById('personAge').innerHTML = fname + " er " + age + " år gammel";
-    document.getElementById('message').innerHTML = message;
-  });
-  
+    const currentYear = new Date().getFullYear();
+    return currentYear - this.fodselsar;
+  }
+  under() {
+    const age = this.calculateAge();
+    return age <= 15;
+  }
+  mellom() {
+    const age = this.calculateAge();
+    return age >= 15 && age <= 18;
+  }
+  over() {
+    const age = this.calculateAge();
+    return age >= 18;
+  }
+}
+
+document.getElementById('calculatorInput').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const fname = document.getElementById('fname').value;
+  const fodselsar = document.getElementById('fodselsar').value;
+  const nyAlder = new DinAlder(fname, fodselsar);
+  const age = nyAlder.calculateAge();
+
+  document.getElementById("calculatorInput").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        answer();
+    }
+});
+
+  let message = '';
+  if (nyAlder.mellom()) {
+    message = fname + ' er eldre enn den kriminelle lavalder';
+  }
+  if (nyAlder.under()) {
+    message = fname + ' er yngre enn den kriminelle lavalder';
+  }
+  if (nyAlder.over()) {
+    message = fname + ' er over enn den kriminelle lavalder og du har stemmerett';
+  }
+
+  document.getElementById("personAge").style.display = "flex";
+  document.getElementById("message").style.display = "flex";
+  document.getElementById('personAge').innerHTML = fname + " er " + age + " år gammel";
+  document.getElementById('message').innerHTML = message;
+});
