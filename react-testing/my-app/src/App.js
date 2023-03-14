2a5194cceb99a50d84227904401fd10414c92ae8
thammer <tobias@hammerhome.no>
2023-03-14 14:51:15 +0100

    different chats and other

--- react-testing/my-app/src/App.js
+++ react-testing/my-app/src/App.js
@@ -1,12 +1,35 @@
 import React, { useState, useCallback } from 'react';
 import './App.css';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+
 function ChatApp() {
   const [inputValue, setInputValue] = useState('');
-  const [messages, setMessages] = useState([]);
+  const [chats, setChats] = useState({
+    chat1: {
+      member1: [],
+      member2: [],
+      member3: [],
+    },
+    chat2: {
+      member1: [],
+      member2: [],
+      member3: [],
+    },
+    chat3: {
+      member1: [],
+      member2: [],
+      member3: [],
+    },
+  });
   const [currentChat, setCurrentChat] = useState('chat1');
   const [members, setMembers] = useState(['member1', 'member2', 'member3']);
-  const [chats, setChats] = useState(['chat1', 'chat2', 'chat3']);
+  const [availableChats, setAvailableChats] = useState(['chat1', 'chat2', 'chat3']);
 
   const handleInput = event => {
     setInputValue(event.target.value);
@@ -14,9 +37,21 @@ function ChatApp() {
 
   const handleSubmit = useCallback(event => {
     event.preventDefault();
-    setMessages(prevMessages => prevMessages.concat([inputValue]));
+    setChats(prevChats => {
+      const chatId = currentChat;
+      const memberId = 'member1'; // TODO: Change to the ID of the currently logged-in user
+      const newMessage = inputValue;
+      const updatedChat = {
+        ...prevChats[chatId],
+        [memberId]: [...prevChats[chatId][memberId], newMessage],
+      };
+      return {
+        ...prevChats,
+        [chatId]: updatedChat,
+      };
+    });
     setInputValue('');
-  }, [inputValue]);
+  }, [currentChat, inputValue]);
 
   const handleChatChange = useCallback(event => {
     setCurrentChat(event.target.value);
@@ -24,19 +59,25 @@ function ChatApp() {
 
   return (
     <div className="chat-app">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
       <div className="sidebar">
         <div className="chats">
           <div className="chat-header">
             Chats
           </div>
           <ul>
-            {chats.map(chat => (
+            {availableChats.map(chat => (
               <li key={chat}>
                 <button className={currentChat === chat ? 'active' : ''} value={chat} onClick={handleChatChange}>{chat}</button>
               </li>
             ))}
           </ul>
         </div>
+        
         <div className="members">
           <div className="chat-header">
             Members
@@ -55,13 +96,13 @@ function ChatApp() {
           {currentChat}
         </div>
         <div className="chat-body">
-          {messages.map((message, index) => (
+          {chats[currentChat][members[0]].map((message, index) => (
             <ChatMessage key={index} message={message} />
           ))}
         </div>
         <div className="chat-footer">
           <form className="input-container" onSubmit={handleSubmit}>
-            <input type="text" value={inputValue} onChange={handleInput} maxLength="500"/>
+            <input type="text" value={inputValue} onChange={handleInput} maxLength="500" />
             <button type="submit">Send</button>
           </form>
         </div>
